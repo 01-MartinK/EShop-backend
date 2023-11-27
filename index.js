691fa83eb60a6759b1d9de3159bdf4f4650a5bf9
MK <46935118+TheDeadGamer101@users.noreply.github.com>
2023-11-27 17:15:47 +0200

    Connection to database

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize('mysql://root:qwerty@localhost:3306/joga_sequelize');
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connected to the database');
+    }).catch(err => {
+        console.error('Unable to connect to the database:', err);
+    })
 
 const server = app.listen(PORT, () => {
     console.log(`Server running at ${"http://"+"localhost:"+PORT}`);
